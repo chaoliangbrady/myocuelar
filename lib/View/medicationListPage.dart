0b143f9bf2b510cc9b396386c207aa2f6d7595f3
chaoliang.hsu <chaoliang.hsu@mvpfastlane.com>
2018-12-11 09:00:03 +0800

    refactor medicationPage

--- lib/View/medicationListPage.dart
+++ lib/View/medicationListPage.dart
@@ -6,10 +6,8 @@ class MedicationList extends StatelessWidget {
   Widget build(BuildContext context) {
     return new ListView(
       children: [
-        new Medication('alphagan', 'Alphagan P', 'B', 2, [2, 0], 'OcuelarSmartCap04D1'),
-        new Divider(color: Colors.grey),
-        new Medication('azopt', 'Azopt', 'L', 3, [0, 2, 0], ''),
-        new Divider(color: Colors.grey),
+        new Medication('alphagan', 'Alphagan P', 'B', 2, ['2', '0'], 'OcuelarSmartCap04D1'),
+        new Medication('azopt', 'Azopt', 'L', 3, ['0', '2', '0'], ''),
       ],
     );
   }
